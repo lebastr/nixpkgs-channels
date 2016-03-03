3a8574395660ed116327cecc0421d883594e0986
Alexander Lebedev <lebastr@gmail.com>
2016-03-03 08:20:36 +0000

    amsynth: init at 1.6.4

---
+++ pkgs/applications/audio/amsynth/default.nix
@@ -0,0 +1,21 @@
+{ stdenv, fetchurl, pkgconfig, gtk2, gtkmm, libjack2, alsaLib }:
+
+stdenv.mkDerivation rec {
+    name = "amsynth-${version}";
+    version = "1.6.4";
+
+    src = fetchurl {
+      url = "https://github.com/amsynth/amsynth/releases/download/release-1.6.4/${name}.tar.bz2";
+      sha256 = "07dp9dl38g9krjqxxh89l2z42z08yzrl57cx95b1l67xnxwjp5k3";
+    };
+
+    buildInputs = [ pkgconfig gtk2 gtkmm libjack2 alsaLib ];
+
+    meta = with stdenv.lib; {
+      homepage = http://amsynth.github.io/;
+      description = "amsynth is an easy-to-use software synth with a classic subtractive synthesizer topology";
+      license = licenses.gpl2;
+      maintainers = [ maintainers.lebastr ];
+      platforms = platforms.linux;
+    };
+}
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17257,6 +17257,8 @@ in
 
   antimony = libsForQt5.callPackage ../applications/graphics/antimony {};
 
+  amsynth = callPackage ../applications/audio/amsynth { };
+  
   antiword = callPackage ../applications/office/antiword {};
 
   ao = libfive;
