bd8ff730031a9b1b62bf97b856c8781b1a4db7fc
Alexander <lebastr@gmail.com>
2017-10-22 23:30:36 +0300

    pianoteq: init v581
    pianoteq: 581 -> 602
    
    pianoteq: 602 -> 621

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18015,6 +18015,8 @@ with pkgs;
 
   pianobooster = callPackage ../applications/audio/pianobooster { };
 
+  pianoteq = callPackage ../applications/audio/pianoteq { };
+  
   picard = callPackage ../applications/audio/picard { };
 
   picocom = callPackage ../tools/misc/picocom { };
